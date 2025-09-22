859d664565e0ea2e2cb26731f5b1ec6b986e0d70
Krish Ramena <krish.ramena24@vit.edu>
2025-09-22 11:56:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function updateAllStocks() {
   console.log("Stock data updated at", new Date().toLocaleTimeString());
 }
 
-cron.schedule("*/5 * * * * *", updateAllStocks);
+cron.schedule("*/9 * * * * *", updateAllStocks);
 
 updateAllStocks();
 
